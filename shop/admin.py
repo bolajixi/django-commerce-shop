764d1b2a48e286490bc5406618199c82066514f8
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-02-11 21:15:15 +0100

    Register `Products` model to admin

--- shop/admin.py
+++ shop/admin.py
@@ -8,3 +8,13 @@ class CategoryAdmin(admin.ModelAdmin):
 
     list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    '''Admin View for Product'''
+
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}

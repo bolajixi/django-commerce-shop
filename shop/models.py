65741a7da319917368cd623ea80d093cdbcb77cb
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-02-11 20:54:36 +0100

    Add `Category` model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    """Model definition for Category."""
+
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name

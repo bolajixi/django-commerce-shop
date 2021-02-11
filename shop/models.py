b15a13f2a8c4c1e424fdda90a0744d920a4d322b
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-02-11 20:59:56 +0100

    Add `Products` model

--- shop/models.py
+++ shop/models.py
@@ -13,3 +13,25 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Products(models.Model):
+    """Model definition for Products."""
+
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name

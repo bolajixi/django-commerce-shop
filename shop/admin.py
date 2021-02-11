692e194835ee11f69ee3f086722da1ec41718f77
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-02-11 21:14:28 +0100

    Register `Category` model to admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from shop.models import Category, Products
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    '''Admin View for Category'''
+
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}

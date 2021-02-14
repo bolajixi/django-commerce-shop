21205f3b2a73b642ea75f4f7ca20a926c0feb9fb
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-02-14 01:09:31 +0100

    Fix typo

--- shop/urls.py
+++ shop/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('<slug:category_slug>/', views.product_list,
          name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail,
-         name='product_detaik'),
+         name='product_detail'),
 ]

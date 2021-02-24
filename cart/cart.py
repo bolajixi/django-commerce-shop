1b4628ae55e7afcfe82d115eff1b1c9f43454d01
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-02-24 16:38:02 +0100

    Add `len` method to Cart class

--- cart/cart.py
+++ cart/cart.py
@@ -58,3 +58,9 @@ class Cart(objects):
             item['price'] = Decimal(item['price'])
             item['total_price'] = item['price'] * item['quantity']
             yield item
+
+    def __len__(self):
+        """
+        Count all items in the cart.
+        """
+        return sum(item['quantity'] for item in self.cart.values())

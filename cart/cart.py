3e9f67ac9420344230c8b83faf06899fdef94149
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-02-24 16:18:25 +0100

    Add `add` method to Cart class

--- cart/cart.py
+++ cart/cart.py
@@ -14,3 +14,17 @@ class Cart(objects):
             # save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+
+    def add(self, product, quantity=1, override_quantity=False):
+        """
+        Add a product to the cart or update its quantity.
+        """
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0,
+                                     'price': str(product.price)}
+        if override_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+            self.save()

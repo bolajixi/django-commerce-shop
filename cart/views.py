4c5cffa604ab82d9c133b2d8ebe25f35599edf94
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-02-25 20:01:01 +0100

    Add cart detail view

--- cart/views.py
+++ cart/views.py
@@ -26,3 +26,8 @@ def cart_remove(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
     return redirect('cart:cart_detail')
+
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})

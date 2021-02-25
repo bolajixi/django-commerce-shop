15e4a0d724e78771400add7ac0a0f97888b2aa87
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-02-25 21:23:06 +0100

    Add products to cart feature

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from shop.models import Product, Category
+from cart.forms import CartAddProductForm
 
 # Create your views here.
 
@@ -19,4 +20,6 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop/product/detail.html', {'product': product})
+    cart_product_form = CartAddProductForm()
+    return render(request, 'shop/product/detail.html', {'product': product,
+                                                        'cart_product_form': cart_product_form})

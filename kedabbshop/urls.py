12e9c95e575b41801879af3ee4d19e5d25a43ec2
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-02-25 20:08:15 +0100

    Include `cart` app url to main url

--- kedabbshop/urls.py
+++ kedabbshop/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop')),
 ]

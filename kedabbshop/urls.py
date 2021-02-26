e24c56f11847da237e37d4bed5df5537ed29dea3
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-02-26 20:39:30 +0100

    Include `orders` url in main urls.py

--- kedabbshop/urls.py
+++ kedabbshop/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
 ]

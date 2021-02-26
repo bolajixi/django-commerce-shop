e7b810cbfd0951bf8eefc7a4eb68b7aeff6d36ed
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-02-26 21:05:02 +0100

    Fix typo

--- orders/forms.py
+++ orders/forms.py
@@ -4,6 +4,6 @@ from .models import Order
 
 class OrderCreateForm(forms.ModelForm):
     class Meta:
-    model = Order
-    fields = ['first_name', 'last_name', 'email', 'address',
-              'postal_code', 'city']
+        model = Order
+        fields = ['first_name', 'last_name', 'email', 'address',
+                  'postal_code', 'city']

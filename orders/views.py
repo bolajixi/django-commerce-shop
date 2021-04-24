1b275945ec5b7bb88e05b67c578446c0ddd614a0
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-04-24 22:10:21 +0100

    Add async email

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from orders.models import OrderItem
 from orders.forms import OrderCreateForm
 from cart.cart import Cart
+from .tasks import order_created
 
 # Create your views here.
 
@@ -19,6 +20,8 @@ def order_create(request):
                                          quantity=item['quantity'])
             # clear the cart
             cart.clear()
+            # launch asynchronous task
+            order_created.delay(order.id)
             return render(request,
                           'orders/order/created.html',
                           {'order': order})

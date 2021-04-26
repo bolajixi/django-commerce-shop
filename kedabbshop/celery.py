2e6931434b1d48572c156ae49f7577da3e92fb3a
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-04-26 16:24:54 +0100

    Fix typo

--- kedabbshop/celery.py
+++ kedabbshop/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kedabbshop.settings')
 
 app = Celery('kedabbshop')
 app.config_from_object('django.conf:settings', namespace='CELERY')
--- orders/tasks.py
+++ orders/tasks.py
@@ -16,6 +16,6 @@ def order_created(order_id):
         f'Your order ID is {order.id}.'
     mail_sent = send_mail(subject,
                           message,
-                          'admin@myshop.com',
+                          'admin@kedabbshop.com',
                           [order.email])
     return mail_sent

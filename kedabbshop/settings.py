be12fa3616d7ca281e7142adad6332fc8e7a263a
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-04-26 01:10:12 +0100

    Set env variable

--- kedabbshop/settings.py
+++ kedabbshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_7y!4#n37ruug^6ijm1hs*%a2_8lcwbavi7%tc#)w2tlhjd*75'
+SECRET_KEY = os.environ.get("PROJECT_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

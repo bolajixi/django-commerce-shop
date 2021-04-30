d8c599f4a9b356a64a160d22905058f8b5ddd048
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-04-30 01:57:58 +0100

    Add url for `/payment/verify/` methothod

--- payment/urls.py
+++ payment/urls.py
@@ -5,6 +5,7 @@ app_name = 'payment'
 
 urlpatterns = [
     path('process/', views.payment_process, name='process'),
+    path('verify/<slug:ref>', views.verify_payment, name='verify'),
     path('done/', views.payment_done, name='done'),
     path('canceled/', views.payment_canceled, name='canceled'),
 ]

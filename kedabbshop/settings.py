91adb9cba2dc473896ab6d22e868cf6584689896
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-07-26 15:02:02 +0100

    Add staticfiles directory to settings

--- kedabbshop/settings.py
+++ kedabbshop/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / STATIC_URL,
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
